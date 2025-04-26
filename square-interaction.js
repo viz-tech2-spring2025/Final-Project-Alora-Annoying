52efbcddb61e6afeefb41a828c80c497089e2888
Alora Zhao <92907366+A3An2@users.noreply.github.com>
2025-04-25 20:56:59 -0400

    Update square-interaction.js

--- square-interaction.js
+++ square-interaction.js
@@ -7,15 +7,15 @@ const infoText = document.querySelector('.info-text');
 // Example Box Content
 const boxContents = {
   box1: {
-    image: '/pic/image 27.png',
+    image: './pic/image 27.png',
     text: 'Pied babblers increase cooperative breeding efforts post-drought, with groups ≥6 members achieving 2.3× higher fledgling success through load-lightening and sentinel behavior. '
   },
   box2: {
-    image: '/pic/florida scrub-jay.png',
+    image: './pic/florida scrub-jay.png',
     text: 'After a drought that reduced juvenile survival rates, the population showed increased adult survival and delayed dispersal of young. This adjustment helped stabilize population numbers despite fewer young birds surviving the initial stages'
   },
   box3: {
-    image: '/pic/Amargosa River Pupfish.png',
+    image: './pic/Amargosa River Pupfish.png',
     text: '​The Amargosa River pupfish (Cyprinodon nevadensis amargosae) exhibits physiological compensation to survive in extreme desert conditions. In habitats like Tecopa Bore, where water temperatures can exceed 36°C, these fish have developed adaptations such as reduced body size and altered morphology, including the loss of pelvic fins, to cope with thermal stress . Additionally, they display phenotypic plasticity, allowing developmental changes in response to environmental conditions, which helps maintain homeostasis and reproductive success in fluctuating habitats.​'
   }
 }
